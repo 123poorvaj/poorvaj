9b3648b85df9b182dfb7f829b32877060b6fb5cf
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-16 20:54:05 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,7 @@
+#include<stdio.h>                     /* header file ,it is use provide input and output operation*/
+
+int main(){                          /* main fuction*/
+    printf("Hello world\n");         /*  printf is used to get output*/
+    return 0;                      /* int main only return integer type data*/
+}
+   
