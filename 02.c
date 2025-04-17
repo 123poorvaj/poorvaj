017448c4fe74856d9359adf05384e2af55e7b1e6
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-17 15:45:17 +0530

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,19 @@
+/*fromate specifiers
+ >  %d  is use to print int data type value 
+ >  %f is use to print float data type value
+ >  %lf is use to print double data type value
+ >  %c   is use to print char data type value*/ 
+
+ #include<stdio.h>
+
+ int main(){
+    int a= 1;
+    float b=2.4;
+    double c=2.45;
+    char d='#';
+    printf( "value of a= %d\n",a);
+    printf("value of b = %f\n",b);
+    printf( "value of c= %lf\n",c);
+    printf("value of d = %c\n",d);
+
+ }
