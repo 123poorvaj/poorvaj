5163aeec3a27c08df026787dfb4da0fa54b0326b
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-18 18:23:48 +0530

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,15 @@
+// learning about how to take input in c program
+// syntax - scanf("control string " , variable list);
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf(" enter the value of a and b");
+    scanf("%d%d",&a,&b);
+    printf(" sum of a and b=%d",a+b);
+    return 0;
+}
+
+
+
+
