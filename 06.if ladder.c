5410a5cf60fb9fd74b32868213108d5735aa4a79
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-20 14:34:36 +0530

    Add files via upload

---
+++ 06.if ladder.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a=60,b=80,c=100;
+    if (a>c)
+    {
+        printf(" a is greater then c\n");
+    }
+    else if (c>a)
+    {
+        printf("c is greater than a\n");
+
+    }
+    else{
+        printf("conditions are not sati");
+    }
+}
