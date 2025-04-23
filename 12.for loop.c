d89f22fa4ee6375cddb9fbc827da67159d6cf0dd
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-23 17:21:12 +0530

    Add files via upload

---
+++ 12.for loop.c
@@ -0,0 +1,14 @@
+//it is control the loop
+//it is finite loo
+//syntax >>  for(initialisation ; condition ; incriment or dicrement)
+
+#include<stdio.h>
+int main(){
+     int a=0;
+    for ( a=0; a< 10;a++)
+    {
+      printf("print %d times\n",a);
+    
+    }
+    
+}
