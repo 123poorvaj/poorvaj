d2662e9858ed7704464b6748a7b07816531845b6
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-04-23 17:20:47 +0530

    Add files via upload

---
+++ 10.whil.c
@@ -0,0 +1,19 @@
+//while loop use to control the loop
+//it is pri-test loop and it also infinet loop.
+/**/
+/*while (condition)
+{
+    code;
+}*/
+
+#include<stdio.h>
+int main(){
+    int a=1;
+
+   while (a<10)
+{
+    printf(" print %d time\n",a);
+    a++; // a=a+1(incriment)
+}
+
+}
